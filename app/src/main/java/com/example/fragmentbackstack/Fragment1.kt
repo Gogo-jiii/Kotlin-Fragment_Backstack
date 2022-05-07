4a30c6ebf9a7e8533bd148e066f93ef782258230
VgionTech <(0$t\U9i9b.{3k*>
2022-05-07 21:14:54 +0530

    init

---
+++ app/src/main/java/com/example/fragmentbackstack/Fragment1.kt
@@ -0,0 +1,31 @@
+package com.example.fragmentbackstack
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_1.*
+
+class Fragment1 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_1, container, false)
+
+        btnGotoSecondFragment.setOnClickListener(View.OnClickListener { gotoSecondFragment() })
+        return view
+    }
+
+    private fun gotoSecondFragment() {
+        val fragment2 = Fragment2()
+        val fragmentManager = fragmentManager
+        val fragmentTransaction = fragmentManager!!.beginTransaction()
+        fragmentTransaction.replace(R.id.container, fragment2, "Fragment2")
+        fragmentTransaction.addToBackStack("Fragment2")
+        fragmentTransaction.commit()
+    }
+}
